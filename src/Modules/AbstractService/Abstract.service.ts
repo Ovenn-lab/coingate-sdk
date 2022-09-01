d480b67f54f1c9a56a5a6ac2de146374434b67a8
domantas <domantas@pop-os.localdomain>
2022-09-01 14:00:19 +0300

    fixes

--- src/Modules/AbstractService/Abstract.service.ts
+++ src/Modules/AbstractService/Abstract.service.ts
@@ -2,11 +2,12 @@ import { BuildPathInput } from './types';
 export class AbstractService {
   protected buildPath({ path, pathEnd, params }: BuildPathInput) {
     if (params) {
+      path += pathEnd || '';
       for (const [key, value] of Object.entries(params)) {
-        path = `${path + pathEnd || ''}`.replace(`:${key}`, value);
+        path = path.replace(`:${key}`, value);
       }
     }
-
+    console.log(path);
     return path;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,22 @@ const client = new BaseClient('P_gkiNVtFZkNy1kCekx6fdwsfCT-dWL21tsxrHh_', true);
 
 const test = async () => {
   try {
-    const testt = await client.refunds.createOrderRefund(32820, {
-      amount: 50,
-      address: '2MxRdYGDdyFiqkMUUksTJ3Qey8sTQPfTii9',
-      currency_id: 1,
-      platform_id: 2,
-      reason: 'for test purposes',
-      email: 'testDrive@gmail.com',
-      ledger_account_id: 'random'
-    });
+    // const testt = await client.refunds.createOrderRefund(32820, {
+    //   amount: 50,
+    //   address: '2MxRdYGDdyFiqkMUUksTJ3Qey8sTQPfTii9',
+    //   currency_id: 1,
+    //   platform_id: 2,
+    //   reason: 'for test purposes',
+    //   email: 'testDrive@gmail.com',
+    //   ledger_account_id: 'random'
+    // });
     // const test = await client.public.test(
     //   "P_gkiNVtFZkNy1kCekx6f dwsfCT-dWL21tsxrHh_"
     // );
+    const testt = await client.public.getExchangeRate({
+      from: 'GBP',
+      to: 'EUR'
+    });
     console.log({ testt });
   } catch (e) {
     console.log(e);
