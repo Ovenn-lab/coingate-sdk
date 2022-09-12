ce5569d05f4378d97efaa60931328017cbbd103e
domas <domantas.va@gmail.com>
2022-09-12 11:13:06 +0300

    client -> added missing methods to get apiKey and Enviroment

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,22 @@
-// import { myPackage } from '../src';
+// import { Client } from '../src';
+// jest.mock('../src');
 
-// describe('index', () => {
-//   describe('myPackage', () => {
-//     it('should return a string containing the message', () => {
-//       const message = 'Hello';
-
-//       const result = myPackage(message);
-
-//       expect(result).toMatch(message);
+// describe('Client', () => {
+//   let client: Client;
+//   beforeAll(() => {
+//     client = new Client();
+//   });
+//   describe('getApiKey method', () => {
+//     test('should return null when there is no api key provided', () => {
+//       const result = client.getApiKey();
+//       expect(result).toBe(null);
 //     });
 //   });
+// describe('myPackage', () => {
+//   it('should return a string containing the message', () => {
+//     const message = 'Hello';
+//     const result = myPackage(message);
+//     expect(result).toMatch(message);
+//   });
+// });
 // });
